9f2db4f18481aff71a0680a47d7d196791c27370
Aaron2 <aaronrea18@gmail.com>
2019-09-15 11:33:15 -0400

    Pushing project I have worked on

---
+++ PresidentsOverTime/main.js
@@ -0,0 +1,68 @@
+// // console.log('b'+'a'+ +'a'+'a').toLowerCase();
+// // <!-- https://www.youtube.com/watch?v=sE08f4iuOhA -->
+// let myChart = document.getElementById('myChart').getContext('2d');
+
+// //  global options
+// Chart.defaults.global.defaultFontFamily='lato',
+// Chart.defaults.global.defaultFontSize=18,
+// Chart.defaults.global.defaultFontColor='orange'
+
+// let massPopChart = new Chart(myChart, {
+// type:'radar', //mutiple types bar horizontal bar pie line dougnut radar polarArea
+// data:{
+// labels:['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge'],
+// datasets:[{
+// label:'Population',
+// data:[6,1.8,1.5,1.05,1.04,.9],
+
+// // backgroundColor:['red','yellow','blue','green','purple'],
+// borderWidth:3,
+// borderColor:'orange',
+// hoverBorderWidth:2,
+// hoverBorderColor:'black'
+// }, 
+// { 
+//     data: [2.82,3.50,4.11,5.02,6.35,.80],
+//     label: "Asia",
+//     borderColor: "red",
+//     borderWidth:3,
+//     hoverBorderWidth:200,
+//     hoverBorderColor:['yellow','red','orange','blue','green']
+    
+//     // fill: false
+//   },
+//   { 
+//     data: [10.82,4.50,7.11,1.02,0],
+//     label: "adfjlk",
+//     borderColor: "#8e5ea2",
+//     // fill: false
+//   },
+// ]
+
+// },
+
+// options:{
+// title:{
+// display:true,
+// text:'Largest Cities in Massachusetts',
+// fontSize:25,
+// },
+// legend:{
+// position:'right',
+// labels:{
+// fontColor:'#000'
+// }
+// },
+// // layout:{
+// //     padding:{
+// //         left:50
+
+// //     }
+
+// // }
+
+// //     tooltips:{
+// // enabeled:false
+// //     }
+// }
+// });
